0eb53fa47c00b58e62c4654495a905e1244900b5
F1r3byte <f1r3byte@yandex.com>
2019-05-14 13:28:10 -1200

    Updates

---
+++ precache-manifest.f95fb940f544030353e59e56de8cdd76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c5a29506501d26b7e29391f46fa3b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b1a444d7e092ec14d8f",
+    "url": "./static/css/main.6de3be60.chunk.css"
+  },
+  {
+    "revision": "93de417d84a09cc40415",
+    "url": "./static/js/2.3d076091.chunk.js"
+  },
+  {
+    "revision": "5b1a444d7e092ec14d8f",
+    "url": "./static/js/main.696fcae4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
